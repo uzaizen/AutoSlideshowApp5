ae3e09306606900bbcfb121e14b754f86a32133b
uzaizen <ushio.zaizen@gmail.com>
2021-06-06 21:07:23 +0900

    Updated

--- app/src/main/java/jp/techacademy/ushio/zaizen/autoslideshowapp5/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ushio/zaizen/autoslideshowapp5/MainActivity.kt
@@ -1,11 +1,161 @@
-package jp.techacademy.ushio.zaizen.autoslideshowapp5
+package jp.techacademy.ushio.zaizen.autoslidesshowapp
 
+import android.Manifest
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSON_REQUEST_CODE = 100
+    private var playswitch = 0
+    private var mTimer: Timer? = null
+    private var mTimerSec = 0.0
+    private var mHandler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            getContentsInfo()
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSON_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSON_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        if (cursor!!.moveToFirst()) {
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            imageView.setImageURI(imageUri)
+        }
+
+        forwardbutton.setOnClickListener {
+            if (playswitch == 0) {
+                if (cursor.moveToNext()) {
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                    imageView.setImageURI(imageUri)
+                } else {
+                    cursor.moveToFirst()
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                    imageView.setImageURI(imageUri)
+                }
+            }
+        }
+
+        backbutton.setOnClickListener {
+            if (playswitch == 0) {
+                if (cursor.moveToPrevious()) {
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                    imageView.setImageURI(imageUri)
+                } else {
+                    cursor.moveToLast()
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                    imageView.setImageURI(imageUri)
+                }
+            }
+
+        }
+
+        playstopbutton.setOnClickListener {
+            if (playswitch == 0) {
+                playswitch = 1
+                mHandler.post {
+                    playstopbutton.text = "停止"
+                    forwardbutton.setTextColor(Color.GRAY)
+                    backbutton.setTextColor(Color.GRAY)
+                }
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        if (cursor.moveToNext()) {
+                            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                            val id = cursor.getLong(fieldIndex)
+                            val imageUri =
+                                ContentUris.withAppendedId(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    id
+                                )
+                            mHandler.post {
+                                imageView.setImageURI(imageUri)
+                            }
+                        } else {
+                            cursor.moveToFirst()
+                            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                            val id = cursor.getLong(fieldIndex)
+                            val imageUri =
+                                ContentUris.withAppendedId(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    id
+                                )
+                            mHandler.post{
+                                imageView.setImageURI(imageUri)
+                            }
+                        }
+
+                    }
+                }, 2000, 2000)
+            } else {
+                playswitch = 0
+                mHandler.post {
+                    playstopbutton.text = "再生"
+                    forwardbutton.setTextColor(Color.BLACK)
+                    backbutton.setTextColor(Color.BLACK)
+                }
+                mTimer!!.cancel()
+            }
+        }
     }
 }
